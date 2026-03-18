9c2d3aa90aa7708085af6294d6763d65ca898c20
mohammad <mohammad@blueoak.io>
2026-03-18 15:35:08 +0100

    refactor: utilize centralized shared extractors via registerSharedExtractors in Cima plugins

--- omarC/MyCimaProvider/src/main/kotlin/com/mycima/MyCimaPlugin.kt
+++ omarC/MyCimaProvider/src/main/kotlin/com/mycima/MyCimaPlugin.kt
@@ -3,10 +3,12 @@ package com.mycima
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.cloudstream.shared.extractors.registerSharedExtractors
 
 @CloudstreamPlugin
 class MyCimaPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(MyCima())
+        registerSharedExtractors()
     }
 }
